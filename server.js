61f2859fbf14bd6c8fa5b57f5813032c8cea90ce
javisalva9 <javisalva9@gmail.com>
2018-09-29 12:20:15 +0200

    deleted issues tutorial stuff

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
-import Issue from './models/Issue';
-
 const app = express();
 const router = express.Router();
 
@@ -28,68 +26,6 @@ connection.once('open', () => {
 
 var products = require('./routes/products.js');
 app.use('/products', products);
-
-
-router.route('/issues/add').post((req, res) => {
-    let issue = new Issue(req.body);
-    issue.save()
-        .then(issue => {
-            res.status(200).json({'issue': 'Added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('Failed to create new record');
-        });
-});
-
-router.route('/issues').get((req, res) => {
-    Issue.find((err, issues) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(issues);
-    });
-});
-
-router.route('/issues/:id').get((req, res) => {
-    Issue.findById(req.params.id, (err, issue) => {
-        if (err)
-            console.log(err);
-        else
-            res.json(issue);
-    })
-});
-
-router.route('/issues/update/:id').post((req, res) => {
-    Issue.findById(req.params.id, (err, issue) => {
-        if (!issue)
-            return next(new Error('Could not load Document'));
-        else {
-            issue.title = req.body.title;
-            issue.responsible = req.body.responsible;
-            issue.description = req.body.description;
-            issue.severity = req.body.severity;
-            issue.status = req.body.status;
-
-            issue.save().then(issue => {
-                res.json('Update done');
-            }).catch(err => {
-                res.status(400).send('Update failed');
-            });
-        }
-    });
-});
-
-router.route('/issues/delete/:id').get((req, res) => {
-    Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
-        if (err){
-          console.log('errordelete', issue);
-          res.json(err);
-        } else {
-          console.log('successdeleteissue', issue)
-          res.json('Removed successfully');
-        }
-    });
-});
 
 app.use('/', router);
 
